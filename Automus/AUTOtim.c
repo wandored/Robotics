19f8a5e24894d069d50993c33cabd1ee1a2338cd
Robert Kinsella <chefk67@gmail.com>
2017-02-24 00:49:31 -0500

    added functions

--- Automus/AUTOtim.c
+++ Automus/AUTOtim.c
@@ -7,64 +7,80 @@
 
 
 static void resetDirection(void)
-	{
-		resetGyro(gyroSensor);
-	}
+{
+	resetGyro(gyroSensor);
+}
+
+static void middleDrive(const int ecount, const int speed)
+{
+	moveMotorTarget(middleDriveMotor, ecount, speed);
+	waitUntilMotorStop(middleDriveMotor);
+}
+
+static void moveClaw(const int ecount, const int speed)
+{
+	moveMotorTarget(leftClawMotor, ecount, speed);
+	moveMotorTarget(rightClawMotor, ecount, speed);
+	waitUntilMotorStop(leftClawMotor);
+}
+
+static void fourWheelDrive(const int ecount, const int speed)
+{
+	moveMotorTarget(leftDriveMotor, ecount, speed);
+	moveMotorTarget(rightDriveMotor, ecount, speed);
+	waitUntilMotorStop(leftDriveMotor);
+}
 
 task main()
 {
-	resetGyro(gyroSensor);
-
-	// Move across the filed and stop 5 inches from the wall
-	moveMotorTarget(middleDriveMotor, -1488, 100); // Move to rail and knock balls off
-	waitUntilMotorStop(middleDriveMotor);
-	//test
-	displayMotorValues(line3, leftDriveMotor);
-
-	//Raise the claw into position
-	moveMotorTarget(leftClawMotor, -180, 100);
-	moveMotorTarget(rightClawMotor, -180, 100);
-	waitUntilMotorMoveComplete(leftClawMotor);
-
-	//Move robot towards rail to pick up balls
-	setMultipleMotors(50, leftDriveMotor, rightDriveMotor);
-	waitUntil(getMotorEncoder(leftDriveMotor) > 950);
-	stopAllMotors();
-
-	//lift claws to remove balls from rail
-	moveMotorTarget(leftClawMotor, -270, 75);
-	moveMotorTarget(rightClawMotor, -270, 75);
-	waitUntilMotorMoveComplete(leftClawMotor);
-	stopAllMotors();
-
-	//Reverse robot back to position
-	moveMotorTarget(leftDriveMotor, -950, 75);
-	moveMotorTarget(rightDriveMotor, -950, 75);
-	waitUntilMotorStop(leftDriveMotor);
-
-	//move robot to cemnter of perimeter in front of scoring zone
-		moveMotorTarget(middleDriveMotor, 770, 75);
-	waitUntilMotorStop(middleDriveMotor);
-	displayMotorValues(line3, leftDriveMotor);
-
-	//Move robot toward scoring zone to deposit balls
-	moveMotorTarget(leftDriveMotor, -480, 50);
-	moveMotorTarget(rightDriveMotor, -480, 50);
-	waitUntilMotorStop(leftDriveMotor);
-
-	//lower claws to deposit balls
-	moveMotorTarget(leftClawMotor, -200, 100);
-	moveMotorTarget(rightClawMotor, -200, 100);
-	waitUntilMotorMoveComplete(leftClawMotor);
-	//stopAllMotors();
+	resetDirection();
+	middleDrive(1488, 100);
+	moveClaw(180, 75);
+	fourWheelDrive(950, 50);
+	moveClaw(-270, 75);
+	fourWheelDrive(-950, 75);
+	middleDrive(770, 75);
+	fourWheelDrive(-480, 50);
+	moveClaw(-200, 100);
 	wait(1, seconds);
-
-	moveMotorTarget(leftClawMotor, 150, 75);
-	moveMotorTarget(rightClawMotor, 150, 75);
-	waitUntilMotorMoveComplete(leftClawMotor);
-	stopAllMotors();
-
-
-
-	stopAllMotors();
+	moveClaw(150, 100);
 }
+
+
+//	//lift claws to remove balls from rail
+//	moveMotorTarget(leftClawMotor, -270, 75);
+//	moveMotorTarget(rightClawMotor, -270, 75);
+//	waitUntilMotorMoveComplete(leftClawMotor);
+//	stopAllMotors();
+
+//	//Reverse robot back to position
+//	moveMotorTarget(leftDriveMotor, -950, 75);
+//	moveMotorTarget(rightDriveMotor, -950, 75);
+//	waitUntilMotorStop(leftDriveMotor);
+
+//	//move robot to cemnter of perimeter in front of scoring zone
+//		moveMotorTarget(middleDriveMotor, 770, 75);
+//	waitUntilMotorStop(middleDriveMotor);
+//	displayMotorValues(line3, leftDriveMotor);
+
+//	//Move robot toward scoring zone to deposit balls
+//	moveMotorTarget(leftDriveMotor, -480, 50);
+//	moveMotorTarget(rightDriveMotor, -480, 50);
+//	waitUntilMotorStop(leftDriveMotor);
+
+//	//lower claws to deposit balls
+//	moveMotorTarget(leftClawMotor, -200, 100);
+//	moveMotorTarget(rightClawMotor, -200, 100);
+//	waitUntilMotorMoveComplete(leftClawMotor);
+//	//stopAllMotors();
+//	wait(1, seconds);
+
+//	moveMotorTarget(leftClawMotor, 150, 75);
+//	moveMotorTarget(rightClawMotor, 150, 75);
+//	waitUntilMotorMoveComplete(leftClawMotor);
+//	stopAllMotors();
+
+
+
+//	stopAllMotors();
+//}
