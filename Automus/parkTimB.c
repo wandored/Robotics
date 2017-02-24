807123e88bad867937c7e4999cc5b158f61e4ca1
Robert Kinsella <chefk67@gmail.com>
2017-02-24 17:50:31 -0500

    using gyro with functions

---
+++ Automus/parkTimB.c
@@ -0,0 +1,38 @@
+
+#pragma config(Sensor, port4,           gyroSensor,     sensorVexIQ_Gyro)
+#pragma config(Motor,  motor1,          leftDriveMotor, tmotorVexIQ, PIDControl, driveLeft, encoder)
+#pragma config(Motor,  motor3,          leftClawMotor, tmotorVexIQ, PIDControl, encoder)
+#pragma config(Motor,  motor7,          rightDriveMotor, tmotorVexIQ, PIDControl, reversed, driveRight, encoder)
+#pragma config(Motor,  motor9,          rightClawMotor, tmotorVexIQ, PIDControl, reversed, encoder)
+#pragma config(Motor,  motor10,         middleDriveMotor, tmotorVexIQ, PIDControl, encoder)
+
+// Park on the bridge from the Blue Scoring Zone starting position.
+
+static void middleDrive(const int ecount, const int speed)
+{
+	moveMotorTarget(middleDriveMotor, ecount, speed);
+	waitUntilMotorStop(middleDriveMotor);
+}
+
+static void moveClaw(const int ecount, const int speed)
+{
+	moveMotorTarget(leftClawMotor, ecount, speed);
+	moveMotorTarget(rightClawMotor, ecount, speed);
+	waitUntilMotorStop(leftClawMotor);
+}
+
+static void fourWheelDrive(const int ecount, const int speed)
+{
+	moveMotorTarget(leftDriveMotor, ecount, speed);
+	moveMotorTarget(rightDriveMotor, ecount, speed);
+	waitUntilMotorStop(leftDriveMotor);
+}
+
+task main()
+{
+	middleDrive(800, 100);
+	fourWheelDrive(-1200, 100);
+	stopAllMotors();
+	wait(10, minutes);  // Holds robot in position
+
+}
